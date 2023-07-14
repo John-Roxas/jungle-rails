d9a24369cd4bebe08e016e3640f85d6dd10bcc63
TheLoneliestTime <john.a.roxas@gmail.com>
2023-07-13 17:55:30 -0700

    fixed the login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+  
+    if @user&.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new
+    end
   end
 end
